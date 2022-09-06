50ba1e3891d02d090d84d04d74d8665f71cf5fa6
Oscar Davalos <44180633+odavalos@users.noreply.github.com>
2022-09-06 15:34:41 -0700

    Update projector.py

--- BarlowTwins/Model/projector.py
+++ BarlowTwins/Model/projector.py
@@ -9,38 +9,38 @@ class Projector(nn.Module):
 
     def __init__(self, latent_dim = 50, projection_sizes=[8192, 8192, 8192],lambd=3.9e-3, scale_factor=1):
 
-    super(Projector, self).__init__();
-    self.zdim = latent_dim;
-    
-    self.proj = nn.Sequential(
-                        nn.Linear(self.zdim, 1024),
-                        nn.BatchNorm1d(1024),
-                        nn.ReLU(),
-                        
-                        nn.Linear(1024, 1024),
-                        nn.BatchNorm1d(1024),
-                        nn.ReLU(),
-                        
-                        nn.Linear(1024, 1024),
-                        nn.BatchNorm1d(1024),
-                        nn.ReLU(),
+        super(Projector, self).__init__();
+        self.zdim = latent_dim;
 
-                        nn.Linear(1024, 1024)
-                                   )
-    
-    
-            
-    def forward(self, x):        
-        """
-        
-        Forward pass of the projector
-        
-        """
+        self.proj = nn.Sequential(
+                            nn.Linear(self.zdim, 1024),
+                            nn.BatchNorm1d(1024),
+                            nn.ReLU(),
 
-        p = self.proj(x)
-        
-        
-        return p
+                            nn.Linear(1024, 1024),
+                            nn.BatchNorm1d(1024),
+                            nn.ReLU(),
+
+                            nn.Linear(1024, 1024),
+                            nn.BatchNorm1d(1024),
+                            nn.ReLU(),
+
+                            nn.Linear(1024, 1024)
+                                       )
+
+
+
+        def forward(self, x):        
+            """
+
+            Forward pass of the projector
+
+            """
+
+            p = self.proj(x)
+
+
+            return p
     
 
         
